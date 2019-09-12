5d6439c2f0f1683a8df14a8d81036513d591f806
zxh <zhangxinghai@livzon.cn>
2019-09-12 14:40:52 +0800

    user-scalable=no

--- tools/version.js
+++ tools/version.js
@@ -1,2 +1,7 @@
-// const fs = require('fs')
-// const path = require('path')
+singhi@1821570753579018.onaliyun.com
+
+AccessKey ID
+LTAI4FhkwzhBwynH5Ht8MfZ4
+
+AccessKeySecret
+RxTvVRMJfGfsEO8EiY3zSWT3Oek4tK
