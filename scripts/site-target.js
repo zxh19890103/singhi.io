0a473fa4c04ce2261b05fb440284caacfa82cac7
zxh <zhangxinghai79@gmail.com>
2020-07-25 15:30:43 +0800

    site tar

--- scripts/site-target.js
+++ scripts/site-target.js
@@ -9,7 +9,8 @@ a.style = `
   text-align: center;
   font-size: 13px;
   color: ${isChinaSite ? "#035C97" : "#E60118"};
+  text-decoration: underline;
 `
 a.textContent = isChinaSite ? "境外站" : "大陆站"
-a.href = isChinaSite ? "https://zhangxinghai.cn" : "https://china.zhangxinghai.cn"
+a.href = (isChinaSite ? "https://zhangxinghai.cn" : "https://china.zhangxinghai.cn") + location.pathname
 document.body.appendChild(a)
