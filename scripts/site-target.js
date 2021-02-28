9073060048988f3924d03b20671dd8923abf9e9c
zxh <zhangxinghai79@gmail.com>
2021-02-28 22:43:10 +0800

    cv

--- scripts/site-target.js
+++ scripts/site-target.js
@@ -1,11 +1,12 @@
 const isChinaSite = location.host.indexOf('china') > -1
 const a = document.createElement('a')
 a.className = "hidden-if-mobi";
-a.style = `
+a.style.cssText = `
   position: fixed;
   top: 12px;
   right: 1rem;
   text-decoration: none;
+  color: #FFF;
 `
 a.innerHTML = isChinaSite ? "<i class='iconfont icon-china'></i>" : "<i class='iconfont icon-global'></i>"
 a.href = (isChinaSite ? "https://zhangxinghai.cn" : "https://china.zhangxinghai.cn") + location.pathname
