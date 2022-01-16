9c9867b108b557f1db5bbc0ecb645752e93f3ad2
singhi <zhangxinghai79@gmail.com>
2022-01-16 20:45:04 +0800

    --

--- tools/repl-img-url.js
+++ tools/repl-img-url.js
@@ -135,7 +135,7 @@ const handle = async (postname) => {
 
   console.log("post:", postname)
 
-  if (postname.indexOf('the-one-you-love') === -1) {
+  if (postname.indexOf('the-distance-and-time') === -1) {
     next()
     return
   }
